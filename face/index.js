00ac07cfc9420c6247913230aa8161a4d192f1ed
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-13 21:30:23 +0200

    Add basic face with arc

--- face/index.js
+++ face/index.js
@@ -1,2 +1,38 @@
+'use strict';
+
 const svg = d3.select('svg');
-svg.style('background-color', 'red');
+
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+const eyeSpacing = 100;
+const eyeYOffset = -70;
+
+const circle =  svg.append('circle')
+    .attr('r', height / 2)
+    .attr('cx', width / 2)
+    .attr('cy', height / 2)
+    .attr('fill', 'yellow')
+    .attr('stroke', 'black');
+
+const leftEye =  svg.append('circle')
+    .attr('r', 30)
+    .attr('cx', width / 2 - eyeSpacing)
+    .attr('cy', height / 2 + eyeYOffset)
+    .attr('fill', 'black');
+
+const rightEye =  svg.append('circle')
+    .attr('r', 30)
+    .attr('cx', width / 2 + eyeSpacing)
+    .attr('cy', height / 2 + eyeYOffset)
+    .attr('fill', 'black');
+
+const g = svg.append('g')
+    .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+const mouth = g.append('path')
+    .attr('d', d3.arc()({
+        innerRadius: 80,
+        outerRadius: 100,
+        startAngle: 0,
+        endAngle: Math.PI
+    }));
