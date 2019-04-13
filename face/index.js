16e2df2f8590f97d4a4daf394f2112d8bc596881
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-13 21:51:55 +0200

    Rise right eyebrow

--- face/index.js
+++ face/index.js
@@ -12,40 +12,43 @@ const eyeBrowHeight = 15;
 const eyeBrowYOffSet = -70;
 
 const g = svg.append('g')
-    .attr('transform', `translate(${width / 2}, ${height / 2})`);
+  .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-const circle =  g.append('circle')
-    .attr('r', height / 2)
-    .attr('fill', 'yellow')
-    .attr('stroke', 'black');
+const circle = g.append('circle')
+  .attr('r', height / 2)
+  .attr('fill', 'yellow')
+  .attr('stroke', 'black');
 
 const eyesG = g.append('g')
-    .attr('transform', `translate(0, ${eyeYOffset})`);
+  .attr('transform', `translate(0, ${eyeYOffset})`);
 
-const leftEye =  eyesG.append('circle')
-    .attr('r', eyeRadius)
-    .attr('cx', -eyeSpacing);
+const leftEye = eyesG.append('circle')
+  .attr('r', eyeRadius)
+  .attr('cx', -eyeSpacing);
 
-const rightEye =  eyesG.append('circle')
-    .attr('r', eyeRadius)
-    .attr('cx', eyeSpacing);
+const rightEye = eyesG.append('circle')
+  .attr('r', eyeRadius)
+  .attr('cx', eyeSpacing);
 
 const leftEyeBrow = eyesG.append('rect')
-    .attr('x', -eyeSpacing -eyeBrowWidth / 2)
-    .attr('y', eyeBrowYOffSet)
-    .attr('width', eyeBrowWidth)
-    .attr('height', eyeBrowHeight);
+  .attr('x', -eyeSpacing - eyeBrowWidth / 2)
+  .attr('y', eyeBrowYOffSet)
+  .attr('width', eyeBrowWidth)
+  .attr('height', eyeBrowHeight);
 
-const rightEyeBrow = eyesG.append('rect')
-    .attr('x', eyeSpacing -eyeBrowWidth / 2)
+const rightEyeBrow = eyesG
+  .append('rect')
+    .attr('x', eyeSpacing - eyeBrowWidth / 2)
     .attr('y', eyeBrowYOffSet)
     .attr('width', eyeBrowWidth)
-    .attr('height', eyeBrowHeight);
+    .attr('height', eyeBrowHeight)
+  .transition().duration(2000)
+    .attr('y', eyeBrowYOffSet - 50);
 
 const mouth = g.append('path')
-    .attr('d', d3.arc()({
-        innerRadius: 150,
-        outerRadius: 170,
-        startAngle: Math.PI / 2,
-        endAngle: Math.PI * 3 / 2
-    }));
+  .attr('d', d3.arc()({
+    innerRadius: 150,
+    outerRadius: 170,
+    startAngle: Math.PI / 2,
+    endAngle: Math.PI * 3 / 2
+  }));
