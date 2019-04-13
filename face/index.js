618b181af383dc6f9e93fa6c63c4e8cc126f98a7
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-13 21:40:36 +0200

    Make code more explicit

--- face/index.js
+++ face/index.js
@@ -6,28 +6,26 @@ const width = +svg.attr('width');
 const height = +svg.attr('height');
 const eyeSpacing = 100;
 const eyeYOffset = -70;
-
-const circle =  svg.append('circle')
-    .attr('r', height / 2)
-    .attr('cx', width / 2)
-    .attr('cy', height / 2)
-    .attr('fill', 'yellow')
-    .attr('stroke', 'black');
-
-const leftEye =  svg.append('circle')
-    .attr('r', 30)
-    .attr('cx', width / 2 - eyeSpacing)
-    .attr('cy', height / 2 + eyeYOffset)
-    .attr('fill', 'black');
-
-const rightEye =  svg.append('circle')
-    .attr('r', 30)
-    .attr('cx', width / 2 + eyeSpacing)
-    .attr('cy', height / 2 + eyeYOffset)
-    .attr('fill', 'black');
+const eyeRadius = 30;
 
 const g = svg.append('g')
     .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+const circle =  g.append('circle')
+    .attr('r', height / 2)
+    .attr('fill', 'yellow')
+    .attr('stroke', 'black');
+
+const eyesG = g.append('g')
+    .attr('transform', `translate(0, ${eyeYOffset})`);
+
+const leftEye =  eyesG.append('circle')
+    .attr('r', eyeRadius)
+    .attr('cx', -eyeSpacing);
+
+const rightEye =  eyesG.append('circle')
+    .attr('r', eyeRadius)
+    .attr('cx', eyeSpacing);
 
 const mouth = g.append('path')
     .attr('d', d3.arc()({
