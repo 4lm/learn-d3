9eafe57b450e351dae2e512b6b46e524bea50a8e
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-15 12:06:56 +0200

    Add rects from data

--- bar-chart/index.js
+++ bar-chart/index.js
@@ -5,9 +5,16 @@ const svg = d3.select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
 
+const render = data => {
+  svg.selectAll('rect').data(data)
+    .enter().append('rect')
+      .attr('width', 300)
+      .attr('height', 300);
+};
+
 d3.csv('data.csv').then(data => {
-    data.forEach(d => {
-        d.population = +d.population * 1000;
-    })
-    console.log(data);
+  data.forEach(d => {
+    d.population = +d.population * 1000;
+  })
+  render(data);
 });
