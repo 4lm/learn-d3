62fd8ca6c668513e0c2333aefbaafc14a5ba8911
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-15 16:47:46 +0200

    Add axis labels

--- bar-chart-2/index.js
+++ bar-chart-2/index.js
@@ -8,7 +8,7 @@ const height = +svg.attr('height');
 const render = data => {
   const xValue = d => d.population;
   const yValue = d => d.country;
-  const margin = { top: 20, right: 40, bottom: 30, left: 200};
+  const margin = { top: 50, right: 40, bottom: 70, left: 200};
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
@@ -35,16 +35,26 @@ const render = data => {
     .selectAll('.domain, .tick line')
       .remove();
 
-  g.append('g').call(xAxis)
-    .attr('transform', `translate(0, ${innerHeight})`)
-    .select('.domain')
-      .remove();
+  const xAxisG = g.append('g').call(xAxis)
+    .attr('transform', `translate(0, ${innerHeight})`);
+
+  xAxisG.select('.domain').remove();
+
+  xAxisG.append('text')
+  .attr('x', innerWidth / 2)
+  .attr('y', 65)
+  .attr('fill', 'black')
+  .text('Population')
 
   g.selectAll('rect').data(data)
     .enter().append('rect')
       .attr('y', d => yScale(yValue(d)))
       .attr('width', d => xScale(xValue(d)))
       .attr('height', yScale.bandwidth());
+
+  g.append('text')
+    .attr('y', -10)
+    .text('Top 10 Most Populous Countries');
 };
 
 d3.csv('data.csv').then(data => {
