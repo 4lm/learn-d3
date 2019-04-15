707b435d6766bd51574e2158f97dec50ab9123f5
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-15 12:56:12 +0200

    Add axis

--- bar-chart/index.js
+++ bar-chart/index.js
@@ -8,7 +8,7 @@ const height = +svg.attr('height');
 const render = data => {
   const xValue = d => d.population;
   const yValue = d => d.country;
-  const margin = { top: 20, right: 20, bottom: 20, left: 20};
+  const margin = { top: 20, right: 40, bottom: 20, left: 100};
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
@@ -18,10 +18,15 @@ const render = data => {
 
   const yScale = d3.scaleBand()
     .domain(data.map(yValue))
-    .range([0, innerHeight]);
+    .range([0, innerHeight])
+    .padding(0.1);
 
   const g = svg.append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  g.append('g').call(d3.axisLeft(yScale));
+  g.append('g').call(d3.axisBottom(xScale))
+    .attr('transform', `translate(0, ${innerHeight})`);
 
   g.selectAll('rect').data(data)
     .enter().append('rect')
