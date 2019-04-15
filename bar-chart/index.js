27ca5be168ce34380885ee2422731d3a8013e379
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-15 12:02:16 +0200

    Add data.csv

--- bar-chart/index.js
+++ bar-chart/index.js
@@ -4,3 +4,10 @@ const svg = d3.select('svg');
 
 const width = +svg.attr('width');
 const height = +svg.attr('height');
+
+d3.csv('data.csv').then(data => {
+    data.forEach(d => {
+        d.population = +d.population * 1000;
+    })
+    console.log(data);
+});
